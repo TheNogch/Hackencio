a064e5e6a4fbdaaec04427d4f9a619bb3d0d8a27
TheNogch <nicogoncha@gmail.com>
2021-06-28 20:39:00 -0400

    BuildAdded

--- Assets/Scripts/Menus/MainMenuController.cs
+++ Assets/Scripts/Menus/MainMenuController.cs
@@ -7,7 +7,7 @@ public class MainMenuController : MonoBehaviour
 {
     public void PlayGame()
     {
-        SceneManager.LoadScene("Level1");
+        SceneManager.LoadScene("Level2");
     }
 
     public void QuitGame()
